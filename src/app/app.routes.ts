671b6f8e93e93ef02ebd29b6b9c84d9fb230777c
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2024-11-19 22:17:46 +0100

    feat(routes): implement lazy loading feat(auth): add AuthGuard for route protection feat(preloading): create custom preloading strategy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,17 @@ import { HomeComponent } from '../pages/home/home.component';
 import { AboutComponent } from '../pages/about/about.component';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  {
+    path: '',
+    loadChildren: () =>
+      import('../pages/home/home-routing.module').then((m) => m.HomeRoutingModule),
+    // canActivate: [AuthGuard],
+    // data: { preload: true }
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('../pages/about/about-routing.module').then((m) => m.AboutRoutingModule),
+  },
 ];
+
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.components';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading } from '@angular/router';
 import { APP_ROUTES } from './app/app.routes';
 import { provideStore } from '@ngrx/store';
 import { reducers } from './utils/store/state';
+import { CustomPreloadingStrategy } from './utils/preloading/strategy';
 
-bootstrapApplication(AppComponent,  {providers: [provideRouter(APP_ROUTES), provideStore(reducers)]})
+bootstrapApplication(AppComponent,  {providers: [provideRouter(APP_ROUTES, withPreloading(CustomPreloadingStrategy)), provideStore(reducers),]})
   .catch((err) => console.error(err));
